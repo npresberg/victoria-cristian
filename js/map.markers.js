ee31bfa1a24e6689f992faf332be08cb716901fc
npresberg <npresberg@MacBook-Pro-de-Nadia.local>
2015-09-17 22:18:03 -0300

    jueves 17

--- js/map.markers.js
+++ js/map.markers.js
@@ -7,39 +7,13 @@ var map_markers = [
 		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
 		"infoWindow":"BODEGA SAURUS El Chañar, Neuquén."
 	},
-	{
-		"title": "Accommodation 1",
-		"latitude":33.780428,
-		"longitude":-118.084075,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
-	},
-	{
-		"title": "Accommodation 2",
-		"latitude":33.780428,
-		"longitude":-118.084075,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
-	},
-	{
-		"title": "Accommodation 3",
-		"latitude":33.777551,
-		"longitude":-118.050468,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
-	},
+	
 	{
 		"title": "Transportation",
-		"latitude":33.782930,
-		"longitude":-118.060552,
+		"latitude":-38.9498912,
+		"longitude":-68.1543271,
 		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+		"infoWindow":"Aeropuerto Internacional Neuquén Presidente Juan Domingo Perón"
 	},
-	{
-		"title": "Ceremony",
-		"latitude":33.776384,
-		"longitude":-118.058149,
-		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Birchwood Church <br> Seal Beach, CA 90740"
-	}
+	
 ];
